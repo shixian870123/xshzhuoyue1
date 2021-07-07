1f9551e4bc32b14d509ea9685ee5b779b23ff10d
sansan <sansan@163.com>
2021-07-07 16:13:37 +0800

    查询数据

--- zhuoyue-common-mapper/src/main/java/com/icis/commonmapper/service/IUserInfoService.java
+++ zhuoyue-common-mapper/src/main/java/com/icis/commonmapper/service/IUserInfoService.java
@@ -7,11 +7,11 @@ import java.util.List;
 // 用户接口
 public interface IUserInfoService {
     // 查询所有用户的方法
-    public List<UserInfo> findAllUser();
+     List<UserInfo> findAllUser();
     // 查询用户方法   根据主键查询一个用户数据
-    public UserInfo findUserByPrimaryKey(UserInfo userInfo);
+     UserInfo findUserByPrimaryKey(UserInfo userInfo);
     // 根据  其它字段查询数据
-    public  List<UserInfo> findUserByUserProperty(UserInfo userInfo);
+      List<UserInfo> findUserByUserProperty(UserInfo userInfo);
     // 根据名字 模糊查询用户数据
-    public  List<UserInfo> findUserByLikeName(UserInfo userInfo);
+      List<UserInfo> findUserByLikeName(UserInfo userInfo);
 }
